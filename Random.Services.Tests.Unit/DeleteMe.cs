254a989b07bc72104e3716c3d4972f66e364bb55
Shri Humrudha <shrija@microsoft.com>
2021-11-09 09:48:19 -0800

    INFRA: Initialize Unit Test Project

---
+++ Random.Services.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xunit;
+
+namespace Random.Services.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void DeleteMeTest() =>
+            Assert.True(true);
+    }
+}
