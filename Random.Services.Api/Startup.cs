bd838ef826821d387f1d0d083e112e8fb726262c
Shri Humrudha <shrija@microsoft.com>
2021-11-09 22:03:24 -0800

    CODE RUB: Remove extra line

--- Random.Services.Api/Startup.cs
+++ Random.Services.Api/Startup.cs
@@ -21,7 +21,6 @@ namespace Random.Services.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddTransient<IDateTimeBroker, DateTimeBroker>();
 
