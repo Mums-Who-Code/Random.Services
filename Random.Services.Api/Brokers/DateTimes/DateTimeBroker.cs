ce80e96b4cac1e7fecdafae759bd66eb0085c08b
Shri Humrudha <shrija@microsoft.com>
2021-11-09 19:17:50 -0800

    BROKER: Date Time

---
+++ Random.Services.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+
+namespace Random.Services.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() =>
+            DateTime.UtcNow;
+    }
+}
