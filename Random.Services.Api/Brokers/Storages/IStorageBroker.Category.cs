fd1d5c68f1857c10e46266a5798f83e83deea7a9
Shri Humrudha <shrija@microsoft.com>
2021-12-09 14:13:32 -0800

    BROKER: Insert Category

--- Random.Services.Api/Brokers/Storages/IStorageBroker.Category.cs
+++ Random.Services.Api/Brokers/Storages/IStorageBroker.Category.cs
@@ -2,9 +2,13 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // ------------------------------------------------
 
+using System.Threading.Tasks;
+using Random.Services.Api.Models.Categories;
+
 namespace Random.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Category> InsertCategoryAsync(Category category);
     }
 }
